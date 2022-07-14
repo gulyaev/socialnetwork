727a297379b6bc1782961e577b6197ffea8e5132
gulyaev <gulyaev.gvs@gmail.com>
2022-07-14 19:02:11 +0500

    Redux PostReducer immutable State copy

--- client/src/redux/postsReducer.js
+++ client/src/redux/postsReducer.js
@@ -1,17 +1,25 @@
 const ADD_POST = 'ADD-POST'
 
-const postsReducer = (state, action) => {
+let initialState = [
+    { id: 0, text: "Текст поста 1", likesCount: 5, disLikesCount: 3 },
+    { id: 1, text: "Текст поста 2", likesCount: 4, disLikesCount: 2 },
+    { id: 2, text: "Текст поста 3", likesCount: 3, disLikesCount: 1 }
+]
+
+const postsReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
+            let newState = state.slice()
+            let newPost = {
+                id: Date.now(),
+                text: action.payload,
+                likesCount: 53,
+                disLikesCount: 3
+            }
+            newState.splice(3, 0, newPost)
             return (
-                state.push({
-                    id: Date.now(),
-                    text: action.payload,
-                    likesCount: 53,
-                    disLikesCount: 3
-                })
+                newState
             )
-
         default:
             return state
     }
