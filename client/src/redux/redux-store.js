575a310eddb1bd00b186c7f5c1f8b2b6739af245
gulyaev <gulyaev.gvs@gmail.com>
2022-07-16 00:43:13 +0500

    Redux-devtools-extension

--- client/src/redux/redux-store.js
+++ client/src/redux/redux-store.js
@@ -1,4 +1,6 @@
-import { combineReducers, legacy_createStore as createStore } from 'redux'
+import { combineReducers, legacy_createStore as createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from 'redux-thunk'
 import postsReducer from './postsReducer'
 import dialogsReducer from './dialogsReducer'
 
@@ -7,7 +9,7 @@ let rootReducer = combineReducers({
     dialogsData: dialogsReducer
 })
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 window.store = store;
 
