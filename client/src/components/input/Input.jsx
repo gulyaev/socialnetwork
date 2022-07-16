d59faa8af3ad6711a2f3686e04b4634834e0359d
gulyaev <gulyaev.gvs@gmail.com>
2022-07-16 13:14:39 +0500

    Forms: Registration, Login, Input.jsx

---
+++ client/src/components/input/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Input = (props) => {
+    return (
+        <input type={props.type} placeholder={props.placeholder}
+            value={props.value} onChange={(e) => { props.setValue(e.target.value) }} />
+    )
+}
+
+export default Input
