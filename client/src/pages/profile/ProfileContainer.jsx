84065adbc54a68a77324717c99c381d0e64b2f42
gulyaev <gulyaev.gvs@gmail.com>
2022-07-19 14:17:08 +0500

    WithRouter Profile getting UserId from URL

--- client/src/pages/profile/ProfileContainer.jsx
+++ client/src/pages/profile/ProfileContainer.jsx
@@ -1,6 +1,40 @@
 import Profile from "./Profile"
+import React from "react"
 import {connect} from "react-redux"
 import { setCurrentUserActionCreator } from "../../redux/usersPageReducer"
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+class ProfileContainer extends React.Component {
+    componentDidMount() {
+        const userId = this.props.params.id
+        // this.props.setIsFetching(true)
+        axios.get(`http://localhost:5000/api/user/${userId}`).then(res => {
+        //     this.props.setIsFetching(false)
+            console.log(res.data)
+            this.props.setCurrentUser(res.data)
+        })
+    }
+
+    render = () => {
+        return (
+            <Profile currentUser = {this.props.currentUser} />
+        )
+    }
+}
+
+const withRouter = WrappedComponent => props => {
+    const params = useParams();
+    // etc... other react-router-dom v6 hooks
+  
+    return (
+      <WrappedComponent
+        {...props}
+        params={params}
+        // etc...
+      />
+    );
+  };
 
 let mapStateToProps = (state) => {
     return {
@@ -10,10 +44,10 @@ let mapStateToProps = (state) => {
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        setCurrentUser: (userData) => dispatch(setCurrentUserActionCreator(userData)),
+        setCurrentUser: (userData) => dispatch(setCurrentUserActionCreator(userData))
     }
 }
 
-const ProfileContainer = connect(mapStateToProps, mapDispatchToProps)(Profile)
+let WithURLDataContainerComponent = withRouter(ProfileContainer)
 
-export default ProfileContainer
+export default connect(mapStateToProps, mapDispatchToProps)(WithURLDataContainerComponent)
